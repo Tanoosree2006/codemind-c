cbdd4e0c00adac12a357acf9c259b932e5d01140
Tanoosree2006 <147473260+Tanoosree2006@users.noreply.github.com>
2024-03-08 13:11:20 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int arr[n];     for(int i=0;i<=n;i++)     {         scanf("%d",&arr[i]);     }     int min=0;     for(int i=0;i<n;i++)     {         if(arr[i]%2!=0&&i%2==0){
                min++;
            }
        }
        if(min>=1)
        {
            printf("False");
        }
        else
        {
            printf("True");
        }
    }

---
+++ Strictly_ODD.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<=n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int min=0;
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]%2!=0&&i%2==0){
+            
+            min++;
+        }
+    }
+    if(min>=1)
+    {
+        printf("False");
+    }
+    else
+    {
+        printf("True");
+    }
+}
